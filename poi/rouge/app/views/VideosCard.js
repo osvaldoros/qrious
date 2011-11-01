0ddafe46b5bfd99ab5a8f127ec835b6f2e9db437
pnykamp <pnykamp@c8e7dfc9-19e0-8d44-a8c7-f283ddc565b6>
2011-11-01 23:29:35 +0000

    made content urls in style and videosCard absolute so they will work in any project urls in index use the local server root so updated code will run Osvaldo please test to see if your project runs properly ;-)
    git-svn-id: http://localhost/svn/qrious@97 c8e7dfc9-19e0-8d44-a8c7-f283ddc565b6

--- poi/rouge/app/views/VideosCard.js
+++ poi/rouge/app/views/VideosCard.js
@@ -16,10 +16,10 @@ app.views.VideosCard = Ext.extend(Ext.Panel, {
 		},
 		{
 	        xtype: 'video',
-	        url: '/poi/rouge/videos/rouge_white.m4v',
+	        url: 'http://qriousmobile.com/poi/rouge/videos/rouge_white.m4v',
 	        width: 300,
 	        height: 150,
-	        posterUrl: '/poi/rouge/videos/rouge_white_poster.png'
+	        posterUrl: 'http://qriousmobile.com/poi/rouge/videos/rouge_white_poster.png'
 		},
 	]
 });
