ff1c070ada02aeff48e15d8a97740b0d65968fe4
pnykamp <pnykamp@c8e7dfc9-19e0-8d44-a8c7-f283ddc565b6>
2011-11-15 19:48:17 +0000

    reworked the code in VideosCard so the video plays on android phoned. (the old code is still in the file commented out) Modified the solution found in this post to work with our presentation. http://www.sencha.com/forum/archive/index.php/t-108591.html
    git-svn-id: http://localhost/svn/qrious@104 c8e7dfc9-19e0-8d44-a8c7-f283ddc565b6

--- poi/rouge/app/views/VideosCard.js
+++ poi/rouge/app/views/VideosCard.js
@@ -1,3 +1,95 @@
+/**/
+
+var vidTmp = new Ext.XTemplate(
+		'<tpl for=".">',
+		'<div class="container">',
+		'<div>{title}</div>',
+		'<video id="video{id}" autobuffer width="300" height="150" poster="http://qriousmobile.com/poi/rouge/videos/poster_aster_and_goldenrod.png" >',
+		'<source src="{url}">',
+		'</video>',
+		'</div>',
+		'</tpl>');
+
+Ext.regModel('Video', {
+	fields : ['url', 'id']
+});
+
+var videos = new Ext.data.Store({
+		model : 'Video',
+		data : [{
+				url : 'http://qriousmobile.com/poi/rouge/videos/asters_and_goldenrod.m4v',
+				id : '1'
+			}
+		]
+	});
+
+var pnl = new Ext.DataView({
+		title : 'Video',
+		store : videos,
+		tpl : vidTmp,
+		itemSelector : '.container',
+		listeners : {
+			itemtap : function (view, index, el, e) {
+				rec = videos.getAt(index);
+				video = document.getElementById('video' + rec.data.id);
+				video.play();
+				
+			}
+		},
+		fullscreen : true
+	})
+
+
+var panel = new Ext.Panel({
+    width: 300,
+    height: 150,
+
+    items: new Ext.DataView({
+		title : 'Video',
+		store : videos,
+		tpl : vidTmp,
+		itemSelector : '.container',
+		listeners : {
+			itemtap : function (view, index, el, e) {
+				rec = videos.getAt(index);
+				video = document.getElementById('video' + rec.data.id);
+				video.play();
+				
+			}
+		},
+		fullscreen : true
+	})
+});
+
+
+
+app.views.VideosCard = Ext.extend(Ext.Panel, {
+	title: "Videos",
+	iconCls: "videos",
+	scroll:'vertical',
+	dockedItems:[
+		{xtype:'Header', title:'Video'},
+	],
+	
+	layout:{
+		type:'vbox',
+		pack:'center',
+	},
+	items:[
+		{
+			height:60,
+		},
+		panel,
+	]
+});
+
+Ext.reg("VideosCard", app.views.VideosCard);
+
+
+
+
+/* this is the original code before it was working on android
+
 app.views.VideosCard = Ext.extend(Ext.Panel, {
 	title: "Videos",
 	iconCls: "videos",
@@ -16,12 +108,17 @@ app.views.VideosCard = Ext.extend(Ext.Panel, {
 		},
 		{
 	        xtype: 'video',
-	        url: 'http://qriousmobile.com/poi/rouge/videos/rouge_white.m4v',
+	        url: 'http://qriousmobile.com/poi/rouge/videos/asters_and_goldenrod.m4v',
 	        width: 300,
 	        height: 150,
-	        posterUrl: 'http://qriousmobile.com/poi/rouge/videos/rouge_white_poster.png'
+	        posterUrl: 'http://qriousmobile.com/poi/rouge/videos/poster_aster_and_goldenrod.png'
 		},
 	]
 });
 
 Ext.reg("VideosCard", app.views.VideosCard);
+
+
+*/
+
+
