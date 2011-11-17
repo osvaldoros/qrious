6e62514170f2d12af7d079dfa038dbc9b49c46ef
oros <oros@c8e7dfc9-19e0-8d44-a8c7-f283ddc565b6>
2011-11-17 05:21:07 +0000

    improved thumbs in gallery
    git-svn-id: http://localhost/svn/qrious@107 c8e7dfc9-19e0-8d44-a8c7-f283ddc565b6

--- poi/rouge/app/views/PhotosCard.js
+++ poi/rouge/app/views/PhotosCard.js
@@ -7,13 +7,24 @@
 	}
 	
 	var thumbnails = {
+		height:'90%',
 		layout:{
 			type:'vbox',
-			pack:'center',
 		},
 		items:[
+			{xtype:'spacer'},
 			{
-				height:20,
+				height:80,
+				layout:{
+					type:'hbox',
+					pack:'center',
+					itemCls:'thumb'
+				},
+				items:[
+					{width:60, height:60, cls: 'thumb1', listeners:{body:{tap:function(event){ switchToGallery(0)	}}}, id:'thumb1'},
+					{width:60, height:60, cls: 'thumb2', listeners:{body:{tap:function(event){ switchToGallery(1)	}}}, id:'thumb2'},
+					{width:60, height:60, cls: 'thumb3', listeners:{body:{tap:function(event){ switchToGallery(2)	}}}, id:'thumb3'},
+				]
 			},
 			{
 				height:80,
@@ -23,13 +34,11 @@
 					itemCls:'thumb'
 				},
 				items:[
-					{width:40, height:40, cls: 'thumb1', listeners:{body:{tap:function(event){ switchToGallery(0)	}}}, id:'thumb1'},
-					{width:40, height:40, cls: 'thumb2', listeners:{body:{tap:function(event){ switchToGallery(1)	}}}, id:'thumb2'},
-					{width:40, height:40, cls: 'thumb3', listeners:{body:{tap:function(event){ switchToGallery(2)	}}}, id:'thumb3'},
-					{width:40, height:40, cls: 'thumb4', listeners:{body:{tap:function(event){ switchToGallery(3)	}}}, id:'thumb4'},
-					{width:40, height:40, cls: 'thumb5', listeners:{body:{tap:function(event){ switchToGallery(4)	}}}, id:'thumb5'},
+					{width:60, height:60, cls: 'thumb4', listeners:{body:{tap:function(event){ switchToGallery(3)	}}}, id:'thumb4'},
+					{width:60, height:60, cls: 'thumb5', listeners:{body:{tap:function(event){ switchToGallery(4)	}}}, id:'thumb5'},
 				]
-			}
+			},
+			{xtype:'spacer'},
 		]
 	}
 	
@@ -39,11 +48,9 @@
 		iconCls: "photos",
 		scroll:'vertical',
 		dockedItems:[
-			{xtype:'Header', title:'Photos'}
-		],
-		items:[
+			{xtype:'Header', title:'Photos'},
 			thumbnails
-		],
+		]
 	});
 	
 	Ext.reg("PhotosCard", app.views.PhotosCard);
