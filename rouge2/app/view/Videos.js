295df871fea4dac13831de7e2f4cfc4c3c2a7040
Osvaldo Ros <osvaldo.ros@gmail.com>
2012-04-04 21:53:26 -0400

    attempt to create the videos tab.. I'm giving up for now it is too much work

--- rouge2/app/view/Videos.js
+++ rouge2/app/view/Videos.js
@@ -1,7 +1,35 @@
+Ext.define('Video', {
+	extend: 'Ext.data.Model',
+	config:{
+	    fields: ['name', 'poster', 'url', 'id'],
+	    proxy:{
+	    	type:'rest',
+	    	url: 'json/videos.json',
+	    	reader: {
+	    		type: 'json',
+	    		rootProperty:'data'
+	    	}
+	    }
+	}
+});
+
+
+var vidTmp2 = new Ext.XTemplate([
+	'<tpl for=".">',
+	'<center>',
+	'<video id="video{id}" width="144" height="82" poster="{poster}" >',
+	'<source src="{url}">',
+	'</video><br>',
+	'{name} <p style="font-size: 10pt">{length}</p>',
+	'</center>',
+	'</tpl>'].join()
+);
+
+	
 Ext.define("Qrious.view.Videos",{
 	extend: 'Ext.Panel',
 	xtype:'videosPanel',
-	
+	requires:['Ext.data.Store'],
 	
 	config: {
 		title: 'Videos',
@@ -10,7 +38,13 @@ Ext.define("Qrious.view.Videos",{
 		
 		html:'Videos',
 		items:[
-			{xtype: 'headerControl', dock: 'top', title:'Videos'}
+			{xtype: 'headerControl', dock: 'top', title:'Videos'},
+			new Ext.List({
+			    itemTpl : vidTmp2,
+			    store: Ext.create('Ext.data.Store',{
+			    	model: 'Video'
+			    })
+			})
 		]
 	}
 	
